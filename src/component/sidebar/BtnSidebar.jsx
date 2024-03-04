800b11c48d98cf6c5a1d7362d9e72a2c646882ea
Kleibert <kleibertmedina@gmail.com>
2024-03-04 11:55:00 -0500

    carrito moificado

--- src/component/sidebar/BtnSidebar.jsx
+++ src/component/sidebar/BtnSidebar.jsx
@@ -1,14 +1,41 @@
-import { Button, Text } from '@chakra-ui/react'
+import { Button, Text, Tooltip, keyframes } from '@chakra-ui/react'
 import {Link} from 'react-router-dom'
+import { useCart } from '../../hook/cartHook'
+
+const animation = keyframes
+    `from {
+    opacity: 0
+}
+to {
+    opacity: 1
+}
+`
+    ;
 
 const BtnSidebar = ({link, nameRoute, icon}) => {
+    const { cart } = useCart()
+    const myAnimate = ` ${animation} both 1s`
+
     return (
         <>
         <Link to={link}>
-            <Button w={'100%'} marginY={1} justifyContent={'space-between'} alignContent={'center'}>
+            <Tooltip label={'Nave de compras'} placement={'left'} >
+            <Button 
+            h={['40px', '60px']}
+            w={['40px', '60px']}
+            position={'relative'} marginY={1} justifyContent={'center'} alignContent={'center'}
+            borderRadius={50}
+            bg={' #58D68D'}
+            color={'#2E4053'}
+            border={'1px solid #2E4053'}
+            >
                 <Text>{nameRoute}</Text>
                 <Text fontSize={'1.5rem'}>{icon}</Text>
+                <Text h={['10px', '20px']} w={['10px', '20px']} position={'absolute'} bg='red' borderRadius={50} display={cart.length ? 'block' : 'none'} top={'0'} right={'0'} fontSize={['.5rem', '1rem']} color={'#FFF'}
+                
+                animation={myAnimate}> {cart.length} </Text>
             </Button>
+            </Tooltip>
         </Link>
         </>
     )
