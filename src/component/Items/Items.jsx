633973e84ae41833916fbc4f30221666c1a2aca5
kleibert medina <kleibertmedina@gmail.com>
2024-07-21 08:31:16 -0500

    zona de contacto mejorada para escritorio

--- src/component/Items/Items.jsx
+++ src/component/Items/Items.jsx
@@ -45,31 +45,6 @@ const Items = ({ image, title, text, diretionY, diretionX, idTema, resaltarText 
                     </AccordionPanel>
                 </AccordionItem>
             </Accordion>
-            {/* <Flex flexDirection={[diretionY, diretionX]} gap={[4, 100]} align={'center'} justify={'center'} bg={'white'} p={[6, 10]} 
-         id={idTema}>
-            <Flex align={'center'} justify={'center'} 
-                h={['350px', '350px']}
-                w={['350px', '350px']}
-                borderRadius={10}
-                overflow={'hidden'}
-                >
-                <Image 
-                src={image}
-                h={'100%'}
-                w={'100%'}
-                objectFit={'cover'}
-                name={title}
-                loading='lazy'
-                alt={title}
-                />
-            </Flex>
-            <Flex flexDirection={'column'} align={'center'} justify={'center'} gap={[2, 6]} >
-                <Heading>{title}</Heading>
-                <Container bg={'whitesmoke'} borderRadius={10} paddingY={2}>
-                    {text}
-                </Container>
-            </Flex>
-        </Flex> */}
         </>
     )
 }
--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -1,20 +1,22 @@
 import { Flex, Image } from '@chakra-ui/react'
-import { PiEnvelopeSimpleOpenFill, PiFacebookLogoFill, PiTiktokLogoFill, PiInstagramLogoFill } from "react-icons/pi";
+import { PiEnvelopeSimpleOpenFill, PiTiktokLogoFill, PiInstagramLogoFill } from "react-icons/pi";
+import { FaFacebook } from "react-icons/fa";
 import BtnLink from '../btn/BtnLink';
 import Logo from '../../assets/img/logo.png'
 const Contact = () => {
     return (
         <>
-            <Flex flexDirection={'column'}>
-                <Flex flexDirection={'column'}paddingY={4}>
+            <Flex flexDirection={'column'} justifyContent={'center'} align={'center'}>
+                <Flex flexDirection={['column', 'row']} paddingY={4} gap={10}>
                     <Image
                         src={Logo}
                         loading='lazy'
                         alt='Logo Munay'
+                        h={['auto', '12rem']}
                     />
                     <Flex flexWrap={'nowrap'} align={'center'} justify={'center'} gap={4}>
                         <BtnLink icon={<PiEnvelopeSimpleOpenFill />} link={'#'} />
-                        <BtnLink icon={<PiFacebookLogoFill />} link={'#'} />
+                        <BtnLink icon={<FaFacebook />} link={'#'} />
                         <BtnLink icon={<PiTiktokLogoFill />} link={'#'} />
                         <BtnLink icon={<PiInstagramLogoFill />} link={'#'} />
                     </Flex>
