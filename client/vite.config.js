bd11c5633972358792f0a8d5e0c4602832a137c3
kleibert medina <kleibertmedina@gmail.com>
2024-10-20 09:42:05 -0500

    vite modificado

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  root: 'client', // Ruta al directorio raíz del proyecto
   plugins: [react()],
-})
+});
