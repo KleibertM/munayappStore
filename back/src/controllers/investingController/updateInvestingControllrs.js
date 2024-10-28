857e4fca2a87bd752cd6645f6bd3c7df7cad9f06
kleibert medina <kleibertmedina@gmail.com>
2024-10-27 19:52:19 -0500

    updateInvest ready

--- back/src/controllers/investingController/updateInvestingControllrs.js
+++ back/src/controllers/investingController/updateInvestingControllrs.js
@@ -1,17 +1,16 @@
 const {Investing} = require('../../db');
 const updateInvestController = async (id, newDataInvest) => {
-    console.log('id controller', id)
+    
     try {
         const invest = await Investing.findByPk(id);
         if (!invest) {
             console.error(`El invest con investing_id ${id} no existe en la base de datos.`);
             throw new Error('El inversión no existe');
         }
-        await Investing.update(newDataInvest, {
+        await Investing.update(newDataInvest[0], {
             where: { investing_id: id } // Ajusta el campo si tienes un nombre de clave primaria diferente
         });
         const updatedInvest = await Investing.findByPk(id);
-
         return { message: "Datos actualizados exitosamente", data: updatedInvest };
     } catch (error) {
         console.error("Error al actualizar invest: controller", error.message);
