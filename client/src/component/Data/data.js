a18c9ac3130fb120f442c990fab4ffed85dde66c
kleibert medina <kleibertmedina@gmail.com>
2024-12-27 21:51:31 -0500

    new  description of Data

--- client/src/component/Data/data.js
+++ client/src/component/Data/data.js
@@ -14,7 +14,7 @@ export const dataMunay = [
   {
     id: "1",
     nombre: "Maracuya",
-    descripcion: "Delicioso marciano de maracuya cremoso.",
+    descripcion: "¡Disfruta nuestros irresistibles chupetes de maracuyá! Hechos con maracuyá fresca, agua de botella y azúcar rubia, son bajos en grasa, sin conservantes artificiales, y te ofrecen una explosión de sabor natural y energía. ¡Ricos en antioxidantes que ayudan a proteger tus células!",
     precio: 0.90,
     ingredientes: ["Maracuya", "Azucar Rubia", "Agua botella/hervida"],
     image: ['https://i.ibb.co/svFzjHn/6.jpg'],
@@ -24,7 +24,7 @@ export const dataMunay = [
   {
     id: "2",
     nombre: "Fresa",
-    descripcion: "Delicioso marciano de fresa con trozos de fresas frescas.",
+    descripcion: "Disfruta nuestros deliciosos chupetes de fresa! Hechos con fresa fresca, agua y azúcar rubia, y decorados con un trozo de fresa en el interior. Son bajos en grasa y sin conservantes artificiales. ¡Llenos de vitamina C para fortalecer tu sistema inmunológico!.",
     precio: 0.90,
     ingredientes: ["Fresa", "Azucar Rubia", "Agua botella/hervida"],
     image: ['https://i.ibb.co/W0GqXHr/4.jpg'],
@@ -34,7 +34,7 @@ export const dataMunay = [
   {
     id: "3",
     nombre: "Mango",
-    descripcion: "Delicioso marciano de mango.",
+    descripcion: "¡Saborea nuestros refrescantes chupetes de mango! Hechos con mango natural, agua de botella y azúcar rubia, son bajos en grasa y sin conservantes artificiales. Disfruta una explosión de sabor tropical y energía natural en cada bocado. ¡Ricos en vitamina A para una piel saludable!",
     precio: 0.90,
     ingredientes: ["Mango", "Azucar Rubia", "Agua botella/hervida"],
     image: ['https://i.ibb.co/HX1XNBS/8.jpg'],
@@ -44,7 +44,7 @@ export const dataMunay = [
   {
     id: "6",
     nombre: "Choco Menta",
-    descripcion: "Delicioso marciano de chocolate con menta cremoso.",
+    descripcion: "¡Disfruta nuestros irresistibles chupetes de chocomenta! Con esencia de menta, chocolate derretido, leche evaporada, agua de botella y azúcar rubia, son bajos en grasa y sin conservantes artificiales. ¡Refrescantes, cremosos y deliciosos, perfectos para cualquier ocasión! ¡La menta ayuda a mejorar la digestión!",
     precio: 1.10,
     ingredientes: ["Menta (E)", "Chocolate", "Azucar rubia", "Leche evaporada", "Agua botella/hervida"],
     image: ['https://i.ibb.co/3m7bnwT/1.jpg'],
@@ -54,7 +54,7 @@ export const dataMunay = [
   {
     id: "7",
     nombre: "Lucuma c/ Leche",
-    descripcion: "Delicioso marciano de lucuma cremoso.",
+    descripcion: "¡Disfruta nuestros deliciosos chupetes de lúcuma! Hechos con lúcuma fresca, leche evaporada, agua de botella y azúcar rubia. Son bajos en grasa y sin conservantes artificiales. Descubre el sabor único y nutritivo de la lúcuma en cada bocado. ¡Rica en betacaroteno para una visión saludable!",
     precio: 1.10,
     ingredientes: ["Lucuma", "Azucar rubia", "Leche evaporada", "Agua botella/hervida"],
     image: ['https://i.ibb.co/FBn2B7k/3.jpg'],
@@ -64,7 +64,7 @@ export const dataMunay = [
   {
     id: "4",
     nombre: "Coco ",
-    descripcion: "Delicioso marciano de coco con trozos de coco cremoso.",
+    descripcion: "Disfruta nuestros exquisitos chupetes de coco! Hechos con coco fresco, agua de botella, leche evaporada y azúcar refinada. Son bajos en grasa y sin conservantes artificiales. Deléitate con un sabor tropical y cremoso en cada bocado. ¡El coco es una buena fuente de fibra dietética!",
     precio: 1.10,
     ingredientes: ["Coco", "Azucar refinada", "Leche evaporada", "Agua botella/hervida"],
     image: ['https://i.ibb.co/p46vBgD/7.jpg'],
@@ -74,7 +74,7 @@ export const dataMunay = [
   {
     id: "5",
     nombre: "Oreo",
-    descripcion: "Delicioso marciano de oreo cremoso.",
+    descripcion: "¡Disfruta nuestros cremosos chupetes de Oreo! Hechos con auténticas galletas Oreo, leche evaporada, agua de botella y azúcar rubia. Son bajos en grasa y sin conservantes artificiales. ¡La combinación perfecta de dulzura y cremosidad, aportando una experiencia indulgente y satisfactoria!",
     precio: 1.10,
     ingredientes: ["Oreo", "Azucar rubia", "Leche evaporada", "Agua botella/hervida"],
     image: ['https://i.ibb.co/w0HmZC2/5.jpg'],
@@ -84,7 +84,7 @@ export const dataMunay = [
   {
     id: "8",
     nombre: "Fresa c/ Leche",
-    descripcion: "Delicioso marciano de Fresa con leche y trozos de fresa.",
+    descripcion: "¡Disfruta nuestros deliciosos chupetes de fresa, hechos con ingredientes naturales y frescos! Cada chupete contiene fresa, leche evaporada, agua de botella y azúcar rubia, con un trozo de fresa como decoración. Son bajos en grasas, ricos en vitamina C y antioxidantes, sin conservantes artificiales, y aportan hidratación y energía natural. ¡Una opción saludable y sabrosa para toda la familia!.",
     precio: 1.10,
     ingredientes: ["Fresa", "Azucar refinada", "Leche evaporada", "Agua botella/hervida"],
     image: ['https://i.ibb.co/x1YmbKQ/2.jpg'],
@@ -94,7 +94,7 @@ export const dataMunay = [
   {
     id: "9",
     nombre: "Ron Pasas",
-    descripcion: "Delicioso marciano de Ron con Pasas y trozos de pasas.",
+    descripcion: "¡Disfruta nuestros sofisticados chupetes de ron con pasas! Hechos con pasas maceradas en ron añejo, ron, agua de botella, leche evaporada, vainilla y azúcar rubia. Son bajos en grasa y sin conservantes artificiales. La combinación perfecta de sabores profundos y cremosidad, con el toque antioxidante de las pasas. ¡Una delicia para adultos que buscan algo especial!",
     precio: 1.10,
     ingredientes: ["Pasas","Ron", "Azucar", "Leche evaporada", "Agua botella/hervida"],
     image: ["https://i.ibb.co/dJZ75jG/rr.jpg"],
@@ -105,7 +105,7 @@ export const dataMunay = [
   {
     id: "10",
     nombre: "Cappuccino",
-    descripcion: "Delicioso marciano de cappuccino.",
+    descripcion: "¡Disfruta nuestros deliciosos chupetes de cappuccino! Hechos con café aromático, leche evaporada, agua y azúcar. Son bajos en grasa y sin conservantes artificiales. ¡El café aporta un impulso de energía natural y una experiencia cremosa y refrescante que los amantes del café no se pueden perder!",
     precio: 1.10,
     ingredientes: ["Cafe", "Azucar", "Leche evaporada", "Agua botella/hervida"],
     image: ["https://i.ibb.co/P4q9X2N/Dise-o-sin-t-tulo.jpg"],
